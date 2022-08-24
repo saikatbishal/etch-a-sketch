3aeb423ca5442d537283729a7b657aeaac45efe8
subratabishal <subratabishal10@gmail.com>
2022-08-25 00:23:29 +0530

    commited

--- app1.js
+++ app1.js
@@ -1,6 +1,8 @@
 let color = `black`;
 let clicked = true;
+let board = document.querySelector('.container');
 
+// POPULATING THE BOARD WHEN THE SCREEN FIRES UP OR THE BOARD SIZE IS CHANGED
 function populateBoard(number)
 {
     let board = document.querySelector('.container');
@@ -13,25 +15,25 @@ function populateBoard(number)
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
-        if (clicked === true) {
-            square.addEventListener('mouseover', colorSquare);
-        }
+        square.addEventListener('mouseover', colorSquare);
         square.style.backgroundColor = `white`;
         square.style.border = "1px solid rgba(220,220,220,0.5)";
         board.insertAdjacentElement("beforeend", square);
     }
 }
 let inputSize = document.querySelector(".size-input").value;
-console.log(inputSize);
+// console.log(inputSize);
 populateBoard(inputSize);
 
 document.querySelectorAll(".square").forEach((sq) => {
     sq.addEventListener("click", () => {
-        console.log("square clicked");
+        console.log("square clicked"+clicked);
         clicked = !clicked;
 })
 })
 
+
+// CHANGE NUMBER OF PIXELS INSIDE THE CANVAS
 function changeSize(input)
 {
     if (input > 2 && input <= 500)
@@ -43,22 +45,28 @@ function changeSize(input)
     }
 } 
 
-
+// SETTING COLOR CHOICE
 function setColor(choice)
 {
     color = choice;
 }
 
 
-function resetBoard()
+function resetBoard() // RESET BUTTON FUNCTIONALITY
 {
     let board = document.querySelector('.container');
     let squares = board.querySelectorAll('.square');
     squares.forEach((square) => square.style.backgroundColor =`white`)
 }
 
-function colorSquare()
+function colorSquare() // COLORING ON PIXELS FUNCTIONALITY
 {
+    let sq = document.querySelectorAll('.square');
+    sq.forEach((square) => {
+        square.addEventListener('click', () => clicked = !clicked)
+    }
+    );
+
     if (clicked){
         if (color === "random")
         {
@@ -69,8 +77,20 @@ function colorSquare()
         }
     }
 }
-
+// ADDING SPLASH SCREEN FUNCTIONALITY
 let splashScreen = document.querySelector('.splash-screen');
 splashScreen.addEventListener('click', () => {
     splashScreen.style.display = 'none';
 })
+
+
+// SAVE IMAHE DRAWN BY THE USER -- NOT WORKING 
+const saveImage = () => {
+    const canvas = document.createElement('canvas');
+    console.log(canvas);
+    const ctx = canvas.getContext('2d');
+    console.log(ctx);
+    canvas.width = canvas.height = `500px`;
+    // ctx.drawImage(board, 0, 0, canvas.width, canvas.height);
+    document.body.appendChild(canvas);
+}
